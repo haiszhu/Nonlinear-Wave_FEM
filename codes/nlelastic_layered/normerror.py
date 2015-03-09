589503353e5820c29e17d389206ee1af213d2671
zhuh1106 <zhuh1106@gmail.com>
2015-03-08 23:45:35 -0700

    able to produce loglog plot, need to modify media and other parameters

--- codes/nlelastic_layered/normerror.py
+++ codes/nlelastic_layered/normerror.py
@@ -23,17 +23,20 @@ reload(setrun)
 outdir='./_output'
 frame=1
 err=[]
+h=[]
 i = 0
 
-for mx in [17280, 1080, 2160, 8640]:
+#for mx in [17280, 1080, 2160, 8640]:
 #for mx in [ 3200, 100, 200, 400, 800, 1600]:
 #for mx in [ 200, 100]:
+for mx in [ 360*16, 180, 360, 720, 360*4, 360*8]:
+
     #Set setrun clawdata (some maybe unnecessary, will see)
     rundata=setrun.setrun('classic')
 
     rundata.clawdata.num_cells[0]=mx
     rundata.clawdata.num_output_times=40	# output_times=1 won't work for high grid resolution
-    rundata.clawdata.tfinal=4.0000e+02
+    rundata.clawdata.tfinal=2.0000e+02
     rundata.write()
     runclaw(xclawcmd='xclaw',outdir=outdir)	# xclaw.exe file produced after make .exe 
 
@@ -54,9 +57,10 @@ for mx in [17280, 1080, 2160, 8640]:
 
     #Compute parameter for error calculation
     dx = 3.6000000e+02/mx
-    reshape_para = 17280/mx
+#    reshape_para = 17280/mx
 #    reshape_para = 3200/mx
 #    reshape_para = 200/mx
+    reshape_para = 360*16/mx
 
 #    print reshape_para
 #    print u.shape
@@ -76,6 +80,35 @@ for mx in [17280, 1080, 2160, 8640]:
     i = i + 1
     error = np.sum(abs(stress_reshape - stress_exact)) * dx/reshape_para
     err.append(error)
+    h.append(dx)
 
 print err
+
+ratio = np.zeros(len(err))
+order = np.zeros(len(err))
+
+for i in range(2,len(err)):
+    ratio[i] = err[i-1]/err[i]
+    order[i] = np.log(abs(ratio[i]))/np.log(abs(h[i-1]/h[i]))
+#print order
+
+
+from pylab import *
+pl.loglog(h[1:len(err)],err[1:len(err)],'o')
+pl.xlim([0.5*min(h[1:len(err)]), 1.5*max(h[1:len(err)])])
+pl.ylim([0.5*min(err[1:len(err)]), 1.5*max(err[1:len(err)])])
+
+Ap = np.ones((len(err)-1,2))
+Ap[:,1] = np.log(h[1:len(err)])
+bp = np.log(err[1:len(err)])
+Kp = np.linalg.lstsq(Ap, bp)[0]
+K = Kp[0]
+p = Kp[1]
+print K,p
+err1 = np.exp(K)*h[1:len(err)]**p
+pl.loglog(h[1:len(err)],err1, 'r')
+
+fname = 'loglog_error.png'
+savefig(fname)
+print 'Created ',fname
 
